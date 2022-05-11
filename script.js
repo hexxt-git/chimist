37c45a911363013289d6f2167130d5fca52043b3
9EED <zeghdns@gmail.com>
2022-05-11 22:35:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,91 +54,23 @@ canvas.addEventListener( 'mouseup', ()=>{
     mouse.z = false
 })
 
-class Object {
-    
-//for a quad    x, y,  'quad',  width, height (square or rectangle)
-//for a circle  x, y, 'circle', radius, angle( in radians )
-
-    constructor(x, y, type, a, b, srokeStyle, fillStyle) {
-        
-        this.x = x;
-        this.y = y;
-
-        this.vx = 0;
-        this.vy = 0;
-
-        this.type = type;
-
-        switch (this.type) {
-            case 'quad':{
-                this.w = a;
-                this.h = b;
-                break
-            }
-            case 'circle':{
-                this.r = a;
-                this.a = b;
-                break
-            }
-            case 'fillCircle':{
-                this.r = a;
-                this.a = b;
-                break
-            }
-        }
-
-        this.srokeStyle = srokeStyle;
-        this.fillStyle = fillStyle;
-
-        this.render = ()=>{
-
-            c.strokeStyle = this.srokeStyle;
-            c.fillStyle = this.fillStyle;
-
-            switch (this.type) {
-                case 'quad': {
-                    c.fillRect(this.x, this.y, this.w, this.h);
-                    break;
-                }
-                case 'circle': {
-                    c.beginPath();
-                    c.arc(this.x, this.y, this.r, 0, this.a, false);
-                    c.stroke();
-                    break;
-                }
-                case 'fillCircle': {
-                    c.beginPath();
-                    c.arc(this.x, this.y, this.r, 0, this.a, false);
-                    c.fill();
-                    c.stroke();
-                    break;
-                }
-
-            }
-        }
-
-        this.update = ()=>{
-            this.x += this.vx
-            this.y += this.vy
-        }
-    }
-}
-
 let length = 5
 length = random( 2, 8, true)
 let step = 40;
 let x = width/2 - ( step * length/1.3)
 let y = height/2
 let verticalLine = Math.sqrt( Math.pow( step, 2) + Math.pow( step, 2) ) * 0.9
-let special = rdm(1)
+let special = !rdm(2)
 let double = false
 let triple = false
 let global = {
     C: 0,
     H: 0,
+    O: 0,
 }
 c.fillRect( x-2, y-2, 4, 4 );
-    for ( let i = 0 ; i <= length ; i++ ){
+
+for ( let i = 0 ; i <= length ; i++ ){ //alkane
     let h = 3;
     if ( i == 0 ){
         c.strokeText( 'CH' + h, x + 2, y + 2)
@@ -164,7 +96,7 @@ c.fillRect( x-2, y-2, 4, 4 );
         }
         c.stroke()
     }
-    if ( triple ){
+    if ( triple ){ //
         h--
         h--
         special++
@@ -193,6 +125,32 @@ c.fillRect( x-2, y-2, 4, 4 );
             c.stroke()
 
         }
+    }
+    if (!rdm(3) & h >= 2 & special == 0){
+        special++
+        h--
+        h--
+        c.beginPath()
+        c.moveTo( x+3, y)
+        if ( i % 2 == 0){
+            c.lineTo( x+3, y+verticalLine)
+        } else {
+            c.lineTo( x+3, y-verticalLine)
+        }
+        c.stroke()
+        c.beginPath()
+        c.moveTo( x-3, y)
+        if ( i % 2 == 0){
+            c.lineTo( x-3, y+verticalLine)
+            c.fillRect( x-2, y-2+verticalLine, 4, 4)
+            c.strokeText( 'O', x , y + verticalLine)
+        } else {
+            c.lineTo( x-3, y-verticalLine)
+            c.fillRect( x-2, y-verticalLine-2, 4, 4)
+            c.strokeText( 'O', x , y - verticalLine)
+        }
+        global.O += 1
+        c.stroke()
     }
     if ( !rdm(4) & h >= 1 ){
         let branch = random( 2, 2, true)
@@ -240,4 +198,5 @@ c.fillRect( x-2, y-2, 4, 4 );
     }
 }
 
-c.strokeText(`C${global.C}H${global.H}`, 15, height-25)
+
+global.O != 0 ? c.strokeText(`C${global.C}H${global.H}O${global.O}`, 15, height-25) : c.strokeText(`C${global.C}H${global.H}`, 15, height-25)
